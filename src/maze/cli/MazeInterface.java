c4dcfc2d9dd80cc7a72c5409088fda71972064d6
Pedro <Pedro@192.168.0.199>
2015-04-01 18:36:56 +0100

    MazeInterface stuffie

--- src/maze/cli/MazeInterface.java
+++ src/maze/cli/MazeInterface.java
@@ -19,6 +19,57 @@ public class MazeInterface {
 		Status s = new Status();
 		char answer;
 		int size;
+
+		readInitialInput(s);
+
+		print_maze(s.getMaze().getMaze());
+
+		/////-----MAIN GAME CYCLE-----/////
+		while (!((s.getHero().getX() == s.getExit().getX() && s.getHero()
+				.getY() == s.getExit().getY()))
+				&& s.getHero().isHeroAlive() == true) {
+
+			/////------READS USER INPUT------/////
+			System.out.println("Press W/A/S/D/E/T "); /* char option */
+			answer = read.next().charAt(0);
+
+			if (answer == 'T' || answer == 't') {
+				if (s.getHero().getnDarts() != 0) {
+
+					System.out.println("Press W/A/S/D ");
+					answer = read.next().charAt(0);
+					s.throwDart(answer);
+					read.nextLine();
+				}
+			}
+			
+		/////------UPDATES BOARD ACCORDINGLY------/////
+			s.updateBoard(answer);
+		
+		/////------PRINTS CURRENT MAZE------/////
+			print_maze(s.getMaze().getMaze());
+			
+		/////-----CHECKS IF GAME IS FINISHED------/////
+			//TODO Matar tudo ao finalizar o jogo
+			if (s.getHero().isArmed()) {
+				if (s.getHero().getX() == s.getExit().getX()
+						&& s.getHero().getY() == s.getExit().getY())
+					break;
+			}		
+		}
+		
+		print_maze(s.getMaze().getMaze());
+		
+		if (!s.getHero().isHeroAlive())
+			System.out.println("The hero has died in battle.");
+
+		System.out.println("\nEND");
+		read.close();
+	}
+
+	private static void readInitialInput(Status s) {
+		char answer;
+		int size;
 		do {
 			System.out.println("What would you like to do? ");
 			System.out.println("1 - Default Maze");
@@ -36,7 +87,8 @@ public class MazeInterface {
 
 				read.reset();
 				do {
-					System.out.println("Pick the size of the board: (The size of the board must be Odd)");
+					System.out
+							.println("Pick the size of the board: (The size of the board must be Odd)");
 					read = new Scanner(System.in);
 					size = read.nextInt();
 
@@ -44,10 +96,12 @@ public class MazeInterface {
 				// STUB FOR THE USER TO DECIDE THE DRAGON CHOICE
 				int dragonChoice;
 				do {
-					System.out.println("Which mode would you like for the dragon? ");
+					System.out
+							.println("Which mode would you like for the dragon? ");
 					System.out.println("1 - Static Dragon");
 					System.out.println("2 - Random Moving Dragon");
-					System.out.println("3 - Random Moving Dragon w/ sleep time");
+					System.out
+							.println("3 - Random Moving Dragon w/ sleep time");
 					read = new Scanner(System.in);
 					dragonChoice = read.nextInt();
 					read.reset();
@@ -63,43 +117,12 @@ public class MazeInterface {
 				break;
 
 		} while (answer != '1' && answer != '2');
-		 print_maze(s.getMaze().getMaze());
-		while (!((s.getHero().getX() == s.getExit().getX() && s.getHero().getY() == s.getExit().getY())) && s.getHero().isHeroAlive() == true) {
-
-			System.out.println("Press W/A/S/D/E/T "); /* char option */
-			answer = read.next().charAt(0);
-
-			if (answer == 'T' || answer == 't') {
-				if (s.getHero().getnDarts() != 0) {
-
-					System.out.println("Press W/A/S/D ");
-					answer = read.next().charAt(0);
-					s.throwDart(answer);
-					read.nextLine();
-				}
-			}
-			s.updateBoard(answer);
-			if (s.getHero().isArmed()) {
-				if (s.getHero().getX() == s.getExit().getX() && s.getHero().getY() == s.getExit().getY())
-					break;
-			}
-
-			print_maze(s.getMaze().getMaze());
-		}
-		print_maze(s.getMaze().getMaze());
-		if (!s.getHero().isHeroAlive())
-			System.out.println("The hero has died in battle.");
-
-		System.out.println("\nEND");
-		read.close();
 	}
 
 	public static void print_maze(char[][] maze) {
 		for (int i = 0; i < maze.length; i++) {
 			for (int j = 0; j < maze[0].length; j++) {
-
 				System.out.print(maze[i][j]);
-
 				System.out.print(" ");
 			}
 			System.out.println("");
@@ -108,25 +131,27 @@ public class MazeInterface {
 
 	public static void defaultMaze(Status status) {
 
-		char maze[][] = { { 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X' }, { 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' }, { 'X', ' ', 'X', 'X', ' ', 'X', ' ', 'X', ' ', 'X' },
-				{ 'X', ' ', 'X', 'X', ' ', 'X', ' ', 'X', ' ', 'X' }, { 'X', ' ', 'X', 'X', ' ', 'X', ' ', 'X', ' ', 'X' }, { 'X', ' ', ' ', ' ', ' ', ' ', ' ', 'X', ' ', 'S' },
-				{ 'X', ' ', 'X', 'X', ' ', 'X', ' ', 'X', ' ', 'X' }, { 'X', ' ', 'X', 'X', ' ', 'X', ' ', 'X', ' ', 'X' }, { 'X', ' ', 'X', 'X', ' ', ' ', ' ', ' ', ' ', 'X' },
+		char maze[][] = { { 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X' },
+				{ 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
+				{ 'X', ' ', 'X', 'X', ' ', 'X', ' ', 'X', ' ', 'X' },
+				{ 'X', ' ', 'X', 'X', ' ', 'X', ' ', 'X', ' ', 'X' },
+				{ 'X', ' ', 'X', 'X', ' ', 'X', ' ', 'X', ' ', 'X' },
+				{ 'X', ' ', ' ', ' ', ' ', ' ', ' ', 'X', ' ', 'S' },
+				{ 'X', ' ', 'X', 'X', ' ', 'X', ' ', 'X', ' ', 'X' },
+				{ 'X', ' ', 'X', 'X', ' ', 'X', ' ', 'X', ' ', 'X' },
+				{ 'X', ' ', 'X', 'X', ' ', ' ', ' ', ' ', ' ', 'X' },
 				{ 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X' } };
 
-		// Maze lab = new Maze(maze);
 		status.getMaze().setMaze(maze);
 		status.setHeroPos(1, 1);
 		status.setExitPos(5, 9);
 		status.setSwordPos(4, 1);
 		status.initDragon('D');
 		status.setDragonPos(3, 1);
-		// criar função que chamada tipo dragons.default() ???
 	}
 
 	public static void randomMaze(Status status, int size) {
 		status.getMaze().setMaze(new char[size][size]);
-		// char[][] maze = new char[size][size]; Maze lab = new Maze(maze);
-		// status.setMaze(lab);
 		status.generate();
 	}
 }
