37ea028d7a7024e540a218764a64520fc6d290b4
Filipe <Filipe@Filipe-PC>
2015-04-12 18:34:52 +0100

    extra

--- src/maze/gui/JMazeOptions.java
+++ src/maze/gui/JMazeOptions.java
@@ -49,8 +49,7 @@ public class JMazeOptions extends JDialog {
 	// dialog.setVisible(true);
 	// } catch (Exception e) {
 	// e.printStackTrace();
-	// }
-	// }
+	// }}
 
 	/**
 	 * Create the dialog.
