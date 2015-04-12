1f88c61b2f0407a09f738fb40d338146838b9b1f
Pedro Costa <Pedro Costa@192.168.0.195>
2015-04-12 17:13:59 +0100

    A culpa é do Filipe.

--- src/maze/logic/Exit.java
+++ src/maze/logic/Exit.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
 public class Exit extends Element implements Serializable{
 	private static final long serialVersionUID = 1L;
 
+	
 	public Exit(int x, int y, char figure) {
 		super(x, y, figure);
 	 
